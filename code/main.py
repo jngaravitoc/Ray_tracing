8babe5e8314fb5e0fa4ecd9ec121c76af45243e6
jngaravitoc <nico.garavito@gmail.com>
2016-02-22 02:21:09 -0700

    update

---
+++ code/main.py
@@ -0,0 +1,92 @@
+"""
+
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+from astropy import constants
+from astropy import units as u
+import warnings
+from scipy.special import gamma
+from scipy.integrate import quad
+from sklearn.neighbors import KDTree
+
+warnings.simplefilter('ignore')
+
+if len(sys.argv)<2:
+    sys.stderr.write("Usage: " + sys.argv[0] + " catalogue.txt"\
+                     " 10000(period) 1(Number of random halos) 1(Number "\
+                     "of random directions) 3(Number of nearest neighbors: env)\n")
+    sys.exit('Error: Put all the input parameters')
+
+
+data = sys.argv[1]
+#P = float(sys.argv[2])
+#N = int(sys.argv[3])
+#K = int(sys.argv[4])
+#env = int(sys.argv[5])
+
+
+# P = ray length
+# N = number of emmiters
+# M = number of traced rays in a radom direction
+
+def reading_data(data):
+    halos = np.loadtxt("../data/" + data)
+    h = 0.7
+    ids = halos[:,0]
+    x = halos[:,1]/h
+    y = halos[:,2]/h
+    z = halos[:,3]/h
+    M = halos[:,4]/h
+    R = halos[:,5]/h
+    return ids, x, y, z, M, R
+
+ids, x, y, z, M, R = reading_data(data)
+ids_hh, x_hh, y_hh, z_hh, D3_mean = host_halos(M, x, y, z, ids)
+x_in, y_in, z_in, id_in, D_env = random_halo(idsh, x_hh, y_hh, z_hh, D3_mean)
+x_cube, y_cube, z_cube, R_cube, M_cube, ids_cube = selecting_halos(x_in, y_in, z_in, 10000, x, y, z, R, M, ids)
+x_out, y_out, z_out = random_direction(x_in, y_in, z_in, 10000.0)
+
+#plt.scatter(x, y, s=0.1, c='k')
+#plt.scatter(x_hh, y_hh, s=5, c='k')
+plt.scatter(x_cube, y_cube, s=1, c='g')
+plt.scatter(x_in, y_in, s=40, c='b')
+plt.scatter(x_out, y_out, s=60, c='r')
+plt.show()
+
+    #NHT = []
+    #NHI = []
+"""
+        #print 'for one emitter'
+        
+	for j in range(K):
+                x_out, y_out, z_out = random_direction(x_in, y_in, z_in, P)
+		babs, xabs, yabs, zabs, R_abs, M_abs, ids_abs = impact_parameter(ids_cube, x_cube, y_cube, z_cube, P, x_in, y_in, z_in, R_cube, M_cube, x_out , y_out, z_out)
+	        #for k in range(len(xabs)):
+                #rint D_env, len(xabs), j 
+                 
+		NH = nh(R_abs, babs)
+                if (len(NH==1) & (NH[0]==0)):
+                   NHT.append(0)
+                   NHI.append(0)
+		else:
+		    A = np.zeros(len(NH))
+		    B = np.zeros(len(NH))
+		    C = np.zeros(len(NH))
+  		    E = np.zeros(len(NH))
+		    T = tvir(M_abs*1e10, Z)
+		    for k in range(len(A)):
+			A[k], B[k], C[k] = ABC(T[k], GUVB, NH[k], NSSH )
+    			E[k] = Eta(A[k], B[k], C[k])
+			#rint E[k], A[k], B[k], C[k], NH[k], E[k]*NH[k]
+			#print E[k]
+		# Some times Lya photons dont find any halos
+		#f (NH.sum() > 0):
+			NHT.append(NH.sum())
+			NHI.append(np.sum(E*NH))
+                        #print 'for one absorber'
+	for i in range(len(NHT)):
+		print  int(id_in), D_env, NHT[i], NHI[i]
+	        
+"""
---
+++ code/neutral_h.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+NSSH = 4.0E-3 # See table 2 of Rahmati et al 2013
+GUVB = 4.5E-13 # See table 2 of Rahmati et al 2013
+#Z = 6.0
+
+def lamda_t(t): # cm3/s equation a6, rahmati et al 2013
+    return 1.17E-10 * t**(0.5) * np.exp(-157809.0/t) / (1.0 + np.sqrt(t/10**5))
+
+def alpha_a(t): # cm3 / s equation a3
+    l = 315614/t
+    return 1.269E-13 * l**(1.503) / (1 + (l/0.522)**(0.47))**1.923
+
+def gammaphot(gammauvb, nh, nhssh):
+    gammap =  gammauvb*(0.98*(1.0+ (nh /nhssh)**(1.64))**(-2.28) + 0.22 * (1 + nh  / nssh)**(-0.84))
+    return gammap
+
+def abc(t, g, nh, nssh):
+    a_a = alpha_a(t)
+    lt = lamda_t(t)
+    gphot = gammaphot(g, nh, nssh)
+    a = a_a + lt
+    b = 2.0*a_a + gphot / nh + lt
+    c = a_a
+    return a, b, c
+
+def eta(a, b, c):
+    eta = (b - np.sqrt(b**2.0 - 4.0*a*c)) / (2.0*a)
+    return eta
+
+def tvir(m, z):
+    t = 2554.0 * (m / 1E6)**(2/3.)*((1 + z) / 31.0)
+    return t
+
