c6ffc9757005d676871e36d2c185864138e311cb
jngaravitoc <jngaravitoc@email.arizona.edu>
2016-02-23 17:47:10 -0700

    0.22 factor

--- code/density_profile.py
+++ code/density_profile.py
@@ -2,7 +2,7 @@ import numpy as np
 from astropy import constants as const
 from astropy import units as u
 from scipy.special import gamma
-
+from scipy.integrate import quad
 #Constants:
 G = const.G
 G = G.to(u.cm**3/u.g/u.s**2)
@@ -12,7 +12,7 @@ Omega_m = 0.32
 Omega_b = 0.0455102
 H_0 = 2.19E-18*u.Hz #1/s
 rho_c = 3*H_0**2 / (8.0*np.pi*G) # critical density
-c = 3.75 # Concentration parameter
+cc = 3.75 # Concentration of the halo a z=6
 mp = const.m_p
 mp = mp.to(u.g)
 
@@ -31,25 +31,29 @@ def integral(c):
     integral_scipy,err = quad(model, 0, c)
     return integral_scipy
 
-def rho_g0(zform): #g/cm3
+def rho_g0(zform, c): #g/cm3
     d_c = 3000.0*Omega_m*(1.0+zform)**3
-    return (((f_gas * d_c * rho_c * Omega_b) / Omega_m) * np.exp(27.* b/2.0)*(np.log(1.0+c) - c/(1.0+c)))/integral(c)
+    return (((f_gas * d_c * rho_c * Omega_b) / Omega_m) * np.exp(27.*b/2.0)*(np.log(1.0+c) - c/(1.0+c)))/integral(c)
 
-def rho(r, z):
+def rho(r, z, c):
     r_s = r_vir/c
     total_rho = rho_g0(z) * np.exp**(-27.0*b/2.0) * (1.0+(r/r_s))**(27.0*b/(2.0*r/r_s))
     nh = rrho / mp
     return total_rho, nh
 
-def nh(rvir, B):
-    rho = rho_g0(6.0)#gm/cm3
+#B is the impact parameter.
+#Column density computation see eq.5 of the document
+def nh(rvir, B, c):
+    rhog0 = rho_g0(6.0, c)#gm/cm3
     B = B*u.kpc
+    B = B.to(u.cm)
     rvir = rvir*u.kpc
+    rvir = rvir.to(u.cm)
     A = -0.178*b + 0.982
     beta = 0.9*b
-    r_c =  rvir / c
-    if len(B)==0:
-        NH = np.zeros(1)
-    else:
-        NH = (np.sqrt(np.pi)*(1/r_c**2.0)**(-3.0*beta/2.0)*(B**2.0+r_c**2.0)**(1/2. - 3*beta/2.) * gamma(-0.5 + 3*beta/2.))/(2*gamma(3*beta/2.))
-    return A*NH*rho/mp
+    r_c =  (rvir / c)*0.22
+    #if len(B)==0:
+    #    NH = np.zeros(1)
+    #else:
+    NH = (np.sqrt(np.pi)*(1/r_c**2.0)**(-3.0*beta/2.0)*(B**2.0+r_c**2.0)**(1/2. - 3*beta/2.) * gamma(-0.5 + 3*beta/2.))/(2*gamma(3*beta/2.))
+    return (A*NH*rhog0/mp).value
